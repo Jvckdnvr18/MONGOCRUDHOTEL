c9d6edb5554e56785deb70d714c118a4046cfbd4
Jack atorda <Jvckdnvr18>
2025-12-17 20:19:26 +0800

    re-upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors'); // 1. Import CORS
 const connectDB = require('./config/db');
 
-// Import new routes
 const roomRoutes = require('./routes/roomRoutes');
 const guestRoutes = require('./routes/guestRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
@@ -12,16 +12,20 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// 2. Enable CORS so your React app can connect
+app.use(cors()); 
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	res.send('Welcome to the Hotel Management API');
+    res.send('Welcome to the Hotel Management API');
 });
-// Removed Git merge conflict markers
-// Mount the new routes
+
+// Mount routes
 app.use('/api', roomRoutes);
 app.use('/api', guestRoutes);
-app.use('/api',bookingRoutes);
+app.use('/api', bookingRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
